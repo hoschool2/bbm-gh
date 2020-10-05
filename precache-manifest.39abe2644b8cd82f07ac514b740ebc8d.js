bc1b5c4e3cf91eb696fcaeaf1e3a5061f09da01f
hoschool2 <hoschool2@gmail.com>
2020-10-05 14:36:13 +0800

    Updates

---
+++ precache-manifest.39abe2644b8cd82f07ac514b740ebc8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cba636e975453a18e8ceb6f2486969a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdb1a0618526cec9dbb1",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "c2dd28502deb697cbda9",
+    "url": "/static/css/main.6fcd35f3.chunk.css"
+  },
+  {
+    "revision": "cdb1a0618526cec9dbb1",
+    "url": "/static/js/2.062ad758.chunk.js"
+  },
+  {
+    "revision": "6a3315418ff229eedef5f19694365f7f",
+    "url": "/static/js/2.062ad758.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2dd28502deb697cbda9",
+    "url": "/static/js/main.c3023a67.chunk.js"
+  },
+  {
+    "revision": "cfae76d24fea3101cc8a",
+    "url": "/static/js/runtime-main.49420e8f.js"
+  }
+]);
